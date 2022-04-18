b6ea7afff7862e9e34eae46ce2d864cf0c08c01c
suzon-ahmmed <info.suzonahamed@gmail.com>
2022-04-18 08:52:49 +0600

    modify footer social media link button  hover design and add  custom colors for all social icon logos

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        //dark background for code block
         'dark-code': '#1e293b',
+        //socil media icon
+        'github': '#171515',
+        'linkedin': '#1d66c2',
+        'facebook': '#2677f2',
+        'twiter': '#2ba1f2',
+        'behance': '#2557ff',
+        'youtube': '#FF0000',
       },
     },
     container: {
       center: true,
     },
-   
+
   },
   plugins: [],
 }
